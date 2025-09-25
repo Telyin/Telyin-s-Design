c6fde3bbb5d3137759beb87c90a9eb6e433402d1
Telyin <dudtkd960@gmail.com>
2025-09-25 18:01:39 +0900

    250925

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -75,4 +75,20 @@ $(document).ready(function(){
     // });
 
     
+        const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: '.book .ctrl_wrap .ctrl_btn .btn_next',
+            prevEl: '.book .ctrl_wrap .ctrl_btn .btn_prev',
+        },
+    });
+
+
 }) //document ready
