415b22f93e1245688b0bf4fce2e77456ebcc4988
Telyin <dudtkd960@gmail.com>
2025-09-26 17:23:11 +0900

    250926 을유문화사 end

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -91,4 +91,34 @@ $(document).ready(function(){
     });
 
 
+
+        const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        // loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        // 	delay: 2500,
+        // 	disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.story .ctrl_wrap .ctrl_btn .btn_next',
+            prevEl: '.story .ctrl_wrap .ctrl_btn .btn_prev',
+        },
+    });
+
+    $('footer .top').on('click', function(){
+        let scrolling = $(window).scrollTop()
+        console.log(scrolling)
+        // $(window).scrollTop(0)
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
 }) //document ready
